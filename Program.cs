bc632af3ce68128acef4eddb1e240369e64bbb89
Samuel Oliveira <S.SamuelS.a.m@hotmail.com>
2024-11-21 20:46:55 -0300

    Estruturação do método "Menu"

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace TextEditor
 {
@@ -6,7 +7,45 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("O que deseja realizar?");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar um novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short opcao = short.Parse(Console.ReadLine()!);
+
+            switch (opcao)
+            {
+                case 1:
+                    Abrir();
+                    break;
+                case 2:
+                    NovoArquivo();
+                    break;
+                case 0:
+                    System.Environment.Exit(0);
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+
+        static void Abrir()
+        {
+
+        }
+
+        static void NovoArquivo()
+        {
+
         }
     }
 }
